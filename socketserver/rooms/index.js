96d7bb05fdb3d0733b083af871c29f11abcda1f0
PedroDiaz417 <eric2022412@gmail.com>
2022-06-19 23:57:58 +1000

    update checkScore module

--- socketserver/rooms/index.js
+++ socketserver/rooms/index.js
@@ -4,6 +4,36 @@
 const { v4 } = require("uuid");
 const { getUserData } = require("../auth");
 const { delay } = require("../../utils");
+const { NiuNiu } = require("../niuniu");
+
+
+// test 
+{
+    //ten small
+    var tempCards = [0, 1, 3, 13, 23];
+    var score = NiuNiu.getScore(tempCards);
+    console.log(score);
+    //FullHouse
+    tempCards = [14, 11, 1, 4, 24];
+    score = NiuNiu.getScore(tempCards);
+    console.log(score);
+    //GoldBull
+    tempCards = [14, 11, 5, 4, 24];
+    score = NiuNiu.getScore(tempCards);
+    console.log(score);
+    //Straight
+    tempCards = [4, 1, 2, 15, 23];
+    score = NiuNiu.getScore(tempCards);
+    console.log(score);
+    //Cattle3
+    tempCards = [4, 1, 2, 13, 23];
+    score = NiuNiu.getScore(tempCards);
+    console.log(score);
+
+    //generateRandomCard
+    let randomCards = NiuNiu.getRandomCards(5);
+    console.log("randomCards", randomCards);
+}
 
 class Room {
     constructor(
@@ -23,7 +53,8 @@ class Room {
         this.readyPlayer = 0;       // ready players of room's players
         this.gamingPlayer = 0;      // gaming players
         this.gameStatus = 0; // 0 is initial, 1 is ready, 2 is gaming, 3 is end
-    }
+        this.gameInterval = setInterval(this.mainloop, [2]);
+    };
     enterRoom(user) {
         if (this.players.findIndex((player) => player.id == user.id) != -1) {
             throw new Error("already enterd");
@@ -61,25 +92,32 @@ class Room {
             this.gameStatus = 0;
         }
     }
+    
+    mainloop() { }
+
+    // destructor
+    destroy() {
+        if (this.gameInterval) clearInterval(this.gameInterval)
+    }
 }
 
 const roomManager = (socket, io) => {
     // create, delete Room
-    const createNewRoom = async (data) => {
+    const createNewRoom = (data) => {
         const { cost, setting, name, maxPlayer } = data;
         var newRoom = new Room(socket, cost, setting, name, maxPlayer);
         global.rooms.push(newRoom);
         return newRoom;
     };
-    const enterRoom = async (roomId) => {
+    const enterRoom = (roomId) => {
         let roomIndex = global.rooms.findIndex((room) => room.id == roomId);
         if (roomIndex == -1) return;
         let room = global.rooms[roomIndex];
         if (!room) throw new Error("Invalid Room");
-        await room.enterRoom(socket);
+        room.enterRoom(socket);
         global.users[socket.id].roomId = roomId;
 
-        await socket.emit("entered room", {
+        socket.emit("entered room", {
             id: room.id,
             creator: getUserData(room.creator.id),
             cost: room.cost,
@@ -91,7 +129,7 @@ const roomManager = (socket, io) => {
 
         room.checkPrepare();
     };
-    const removeFromRooms = async () => {
+    const removeFromRooms = () => {
         // find room that user entered
         let roomId = global.users[socket.id].roomId;
         if (!roomId) return;
@@ -106,13 +144,14 @@ const roomManager = (socket, io) => {
 
         // delete room that no one in it
         if (room.players.length == 0) {
+            room.destroy();
             global.rooms.splice(roomIndex, 1);
         }
 
         socket.emit("outed room");
         room.checkPrepare();
     };
-    const validEnterRoom = async () => {
+    const validEnterRoom = () => {
         if (!global.users[socket.id]) throw new Error("auth error");
         if (global.users[socket.id].roomId != null)
             throw new Error(
@@ -122,7 +161,7 @@ const roomManager = (socket, io) => {
     };
 
     // broad cast Rooms
-    const broadcastRooms = async () => {
+    const broadcastRooms = () => {
         var roomInfos = global.rooms.map((room) => {
             return {
                 id: room.id,
@@ -138,23 +177,23 @@ const roomManager = (socket, io) => {
     };
 
     // socket lisnters
-    socket.on("create room", async (data) => {
+    socket.on("create room", (data) => {
         try {
             validEnterRoom();
             const { cost, setting, name, maxPlayer } = data;
-            var newRoom = await createNewRoom({
+            var newRoom = createNewRoom({
                 cost,
                 setting,
                 name,
                 maxPlayer,
             });
-            await enterRoom(newRoom.id);
+            enterRoom(newRoom.id);
             broadcastRooms();
         } catch (err) {
             console.log("create room error ======> ", err.message);
         }
     });
-    socket.on("enter room", async (data) => {
+    socket.on("enter room", (data) => {
         try {
             validEnterRoom();
             const { id } = data;
@@ -164,7 +203,7 @@ const roomManager = (socket, io) => {
             console.log("enter room ======> ", err.message);
         }
     });
-    socket.on("out room", async () => {
+    socket.on("out room", () => {
         try {
             removeFromRooms();
             broadcastRooms();
@@ -172,15 +211,15 @@ const roomManager = (socket, io) => {
             console.log("out room ======> ", err.message);
         }
     });
-    socket.on("disconnect", async () => {
+    socket.on("disconnect", () => {
         try {
-            await removeFromRooms();
+            removeFromRooms();
             broadcastRooms();
         } catch (err) {
             console.log("disconnect ======> ", err.message);
         }
     });
-    socket.on("get rooms", async () => {
+    socket.on("get rooms", () => {
         try {
             broadcastRooms();
         } catch (err) {
@@ -190,10 +229,11 @@ const roomManager = (socket, io) => {
 };
 
 const gameManager = (socket, io) => {
+
     // functions
-    const randomCard = async (len) => {
+    const randomCard = (len) => {
         var arr = [];
-        for (let i = 0; i < len * 5; ) {
+        for (let i = 0; i < len * 5;) {
             let rand = Math.floor(Math.random() * 39 + 1);
             if (arr.indexOf(rand) === -1 && rand % 10 !== 0) {
                 arr.push(rand);
@@ -202,7 +242,7 @@ const gameManager = (socket, io) => {
         }
         return arr;
     };
-    const getCRoom = async () => {
+    const getCRoom = () => {
         if (!global.users[socket.id]) throw new Error("auth error");
         if (global.users[socket.id].roomId == null)
             throw new Error("you didn't entered any room");
@@ -211,15 +251,15 @@ const gameManager = (socket, io) => {
         );
         return room;
     };
-    const init_cards = async (room) => {
-        let cards = await randomCard(room.readyPlayer);
+    const init_cards = (room) => {
+        let cards = randomCard(room.readyPlayer);
         room.emitPlayers("start game", cards, 0);
     };
 
     // listeners
-    socket.on("get ready", async () => {
+    socket.on("get ready", () => {
         try {
-            let room = await getCRoom();
+            let room = getCRoom();
 
             room.readyPlayer += 1;
             if (
@@ -232,17 +272,17 @@ const gameManager = (socket, io) => {
             console.log("get ready ======> ", err.message);
         }
     });
-    socket.on("room status", async () => {
+    socket.on("room status", () => {
         try {
-            let room = await getCRoom();
+            let room = getCRoom();
             //send current status to user
         } catch (err) {
             console.log("room status ======> ", err.message);
         }
     });
-    socket.on("ready on", async () => {
+    socket.on("ready on", () => {
         try {
-            let room = await getCRoom();
+            let room = getCRoom();
 
             room.gamingPlayer++;
             if (room.gamingPlayer === room.readyPlayer) {
