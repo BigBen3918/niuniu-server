195440db512729bbcca30328fbf10e370bb99af1
logan-smith-cloud <websolution0524@gmail.com>
2022-06-27 12:08:28 +1000

    update :sort

--- socketserver/niuniu/index.js
+++ socketserver/niuniu/index.js
@@ -54,7 +54,7 @@ const NiuNiu = {
         let numSum = 0;
         let numSortedCards = [...sortedCards];
         numSortedCards = numSortedCards.map(card => Number(card) % 10);
-        numSortedCards = numSortedCards.sort();
+        numSortedCards.sort();
         numSortedCards.forEach((i) => {
             counts[i] = (counts[i] || 0) + 1;
             numSum += i
@@ -62,7 +62,7 @@ const NiuNiu = {
         counts = counts.filter((c) => {
             if (c) return c
         });
-        counts = counts.sort();
+        counts.sort();
 
         // activity card 
         let activityCards = [];
@@ -128,7 +128,7 @@ const NiuNiu = {
     getScore: (cards = []) => {
         if (cards.length != 5) throw new Error("invalid cards");
         let sortedCard = [...cards];
-        sortedCard = sortedCard.sort((card1, card2) => Number(card1) % 10 - Number(card2 % 10));
+        sortedCard.sort((card1, card2) => Number(card1) % 10 - Number(card2 % 10));
         let { type, activityCards } = NiuNiu.getType(sortedCard);
         return {
             type: type,
